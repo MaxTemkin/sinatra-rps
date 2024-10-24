52b782001baae54e72d25ddf3e26a3c70ad82905
Max Temkin <Max.Temkin@Gmail.com>
2024-10-24 21:26:35 +0000

    Passing grade

--- app.rb
+++ app.rb
@@ -2,8 +2,66 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get('/rock') do
+  rolls = ["rock", "paper", "scissors"]
+  @their_roll = rolls.sample 
+  @our_roll = "rock"
+  
+
+  if @our_roll == @their_roll
+    @result = "We tied!"
+  elsif @our_roll == "rock" && @their_roll == "scissors"
+    @result = "We won!" 
+  elsif @our_roll == "paper" && @their_roll == "rock"
+    @result = "We won!" 
+  elsif @our_roll == "scissors" && @their_roll == "paper"
+    @result = "We won!" 
+  else
+    @result = "We lost!"
+  end
+
+  erb(:rollresult)
+end
+
+get('/paper') do
+  rolls = ["rock", "paper", "scissors"]
+  @their_roll = rolls.sample
+  @our_roll = "paper"
+
+  if @our_roll == @their_roll
+    @result = "We tied!"
+  elsif @our_roll == "rock" && @their_roll == "scissors"
+    @result = "We won!" 
+  elsif @our_roll == "paper" && @their_roll == "rock"
+    @result = "We won!" 
+  elsif @our_roll == "scissors" && @their_roll == "paper"
+    @result = "We won!" 
+  else
+    @result = "We lost!"
+  end
+
+  erb(:rollresult)
+end
+
+get('/scissors') do
+  rolls = ["rock", "paper", "scissors"]
+  @their_roll = rolls.sample
+  @our_roll = "scissors"
+
+  if @our_roll == @their_roll
+    @result = "We tied!"
+  elsif @our_roll == "rock" && @their_roll == "scissors"
+    @result = "We won!" 
+  elsif @our_roll == "paper" && @their_roll == "rock"
+    @result = "We won!" 
+  elsif @our_roll == "scissors" && @their_roll == "paper"
+    @result = "We won!" 
+  else
+    @result = "We lost!"
+  end
+  
+  erb(:rollresult)
 end
